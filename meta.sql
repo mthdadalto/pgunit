70565e3b99022c75e6b97086a48d0e43b07973cf
smiles <smiles@ctemplar.com>
2021-12-14 01:56:06 +0100

    Added Basic PGUnit Integrity Tests

---
+++ meta.sql
@@ -0,0 +1,43 @@
+/*
+ * PGUnit Integrity Test
+ */
+--@test1
+create function test_case_pgunit_integrity_check_true_is_true ()
+  returns void
+  as $$
+begin
+  perform
+    pgunit.assertTrue (true);
+  raise notice 'PGUnit_Info: True is True -> PGUnit works :)';
+end;
+$$
+language plpgsql;
+
+--@test2
+create function test_case_pgunit_integrity_check_false_IsNot_True_not_Errnous ()
+  returns void
+  as $$
+begin
+  raise notice 'PGUnit_Info: Should fail not erroneous';
+  perform
+    pgunit.assertTrue (false);
+end;
+$$
+language plpgsql;
+
+--@test3
+create function test_case_pgunit_integrity_check_erroneous ()
+  returns void
+  as $$
+begin
+  raise notice 'PGUnit_Info: Should fail erroneous';
+  perform
+    pgunit.assertTrue ('a42');
+end;
+$$
+language plpgsql;
+
+select
+  *
+from
+  pgunit.run_suite ('pgunit_integrity');
